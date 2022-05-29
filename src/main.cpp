f55269a80c3d77e907aa05fb0d2c954b6be047f1
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-05-29 18:12:49 -0400

    latest ada

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include "ada/app.h"
-#include "ada/gl/draw.h"
-#include "ada/gl/meshes.h"
-#include "ada/gl/shader.h"
+
+#include "ada/geom/meshes.h"
 #include "ada/shaders/defaultShaders.h"
+
 #include "ada/scene/light.h"
-#include "ada/scene/camera.h"
 
 #include <iostream>
 
@@ -13,110 +12,34 @@ using namespace ada;
 using namespace glm;
 
 class myApp : public App {
-    Vbo*    geom;
-    Shader  shader;
+    Vbo     myVbo;
+    Shader  myShader;
 
-    Light   light;
-    Camera  cam;
-    float   lat = 180.0f;
-    float   lon = 0.0f;
+    Light   myLight;
+    Camera  myCamera;
 
     void setup() {
-        shader.load(getDefaultSrc(FRAG_DEFAULT_SCENE), 
-                    getDefaultSrc(VERT_DEFAULT_SCENE));
+        myShader = createShader(FRAG_DEFAULT_SCENE, VERT_DEFAULT_SCENE);
 
-        geom = cubeMesh(1.0f).getVbo();
+        myVbo.load( cubeMesh(1.0f) );
 
-        cam.setViewport(width, height);
-        cam.setPosition( vec3(0.0f, 0.0f, -10.0f) );
-        cam.lookAt( glm::vec3(0.0f, 0.0f, 0.0f) );
+        myCamera.setViewport(width, height);
+        myCamera.setPosition( vec3(0.0f, 0.0f, -10.0f) );
+        myCamera.lookAt( glm::vec3(0.0f, 0.0f, 0.0f) );
         
-        light.setPosition( vec3(1.0f,1.0f,1.0f) );
+        myLight.setPosition( vec3(1.0f,1.0f,1.0f) );
 
-        camera(true);
+        setCamera(myCamera);
+        addLight(myLight);
     }
 
     void draw() {
         clear(0.0f);
 
-        glDisable(GL_BLEND);
-        glEnable(GL_DEPTH_TEST);
+        orbitControl();
+        shader(myShader);
+        model( myVbo );
 
-        shader.use();
-        shader.setUniform("u_modelViewProjectionMatrix", cam.getProjectionViewMatrix() );
-        shader.setUniform("u_projectionMatrix", cam.getProjectionMatrix());
-        shader.setUniform("u_normalMatrix", cam.getNormalMatrix());
-        shader.setUniform("u_viewMatrix", cam.getViewMatrix() );
-
-        shader.setUniform("u_camera", -cam.getPosition() );
-        shader.setUniform("u_cameraDistance", cam.getDistance());
-        shader.setUniform("u_cameraNearClip", cam.getNearClip());
-        shader.setUniform("u_cameraFarClip", cam.getFarClip());
-        shader.setUniform("u_cameraEv100", cam.getEv100());
-        shader.setUniform("u_cameraExposure", cam.getExposure());
-        shader.setUniform("u_cameraAperture", cam.getAperture());
-        shader.setUniform("u_cameraShutterSpeed", cam.getShutterSpeed());
-        shader.setUniform("u_cameraSensitivity", cam.getSensitivity());
-        shader.setUniform("u_cameraChange", cam.bChange);
-        shader.setUniform("u_iblLuminance", 30000.0f * cam.getExposure());
-
-        // shader.setUniform("u_lightMatrix", light.getBiasMVPMatrix() );
-        // shader.setUniformDepthTexture("u_lightShadowMap", light.getShadowMap(), .shader.textureIndex++ );
-        if (light.getType() != LIGHT_DIRECTIONAL)
-            shader.setUniform("u_light", light.getPosition());
-        if (light.getType() == LIGHT_DIRECTIONAL || light.getType() == LIGHT_SPOT)
-            shader.setUniform("u_lightDirection", light.direction);
-        if (light.falloff > 0)
-            shader.setUniform("u_lightFalloff", light.falloff);
-        shader.setUniform("u_lightIntensity", light.intensity);
-        shader.setUniform("u_lightColor", light.color);
-
-        shader.setUniform("u_resolution", width, height );
-        shader.setUniform("u_time", time);
-        geom->render( &shader );
-
-    }
-
-    void mouseDragged() {
-        float x = mouseX;
-        float y = mouseY;
-
-        if (x <= 0) x = width;
-        else if (x > width) x = 0; 
-
-        if (y <= 0) y = height - 2;
-        else if (y >= height) y = 2;
-
-        if (x != mouseX || y != mouseY) 
-            setMousePosition(x, y);
-    
-        if (mouseButton == 1) {
-            // Left-button drag is used to rotate geometry.
-            float dist = cam.getDistance();
-
-            float vel_x = movedX;
-            float vel_y = movedY;
-
-            if (fabs(vel_x) < 50.0 && fabs(vel_y) < 50.0) {
-                lat -= vel_x;
-                lon -= vel_y * 0.5;
-                cam.orbit(lat, lon, dist);
-                cam.lookAt(glm::vec3(0.0));
-            }
-        } 
-        else {
-            // Right-button drag is used to zoom geometry.
-            float dist = cam.getDistance();
-            dist += (-.008f * movedY);
-            if (dist > 0.0f) {
-                cam.orbit(lat, lon, dist);
-                cam.lookAt(glm::vec3(0.0));
-            }
-        }
-    }
-
-    void onViewportResize(int _newWidth, int _newHeight) {
-        cam.setViewport(_newWidth, _newHeight);
     }
 
 };
